6f1f59e848ea911ed5f9bd524c7cae4f6fd6b980
YutakaAihara <youguisufanyuan@gmail.com>
2021-08-15 20:17:34 +0900

    ループ処理

--- src/guess_number.java
+++ src/guess_number.java
@@ -5,17 +5,18 @@ public class guess_number {
 	public static void main(String[] args) {
 		Integer answer = new Random().nextInt(10) +1;
 		
-		System.out.print("私が持っている数字はいくつでしょうか？ ");
-		Integer guess = new Scanner(System.in).nextInt();
-		
-		if (answer == guess) {
-			System.out.println("大正解！");
-		}else if (answer > guess) {
-			System.out.println("もっと大きい数字です！");
-		} else {
-			System.out.println("もっと小さい数字です！");
+		while (true) {
+			System.out.print("私が持っている数字はいくつでしょうか？ ");
+			Integer guess = new Scanner(System.in).nextInt();
+			
+			if (answer == guess) {
+				System.out.println("大正解！");
+				break;
+			}else if (answer > guess) {
+				System.out.println("もっと大きい数字です！");
+			} else {
+				System.out.println("もっと小さい数字です！");
+			}
 		}
-		
-		System.out.println("答えは " + answer + " でした！");
 	}
 }
