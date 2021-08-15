c5681f7c7466507c483dd6a5f16cee2ee95667cb
YutakaAihara <youguisufanyuan@gmail.com>
2021-08-15 20:20:57 +0900

    カウントを追加

--- src/guess_number.java
+++ src/guess_number.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class guess_number {
 	public static void main(String[] args) {
 		Integer answer = new Random().nextInt(10) +1;
+		Integer count = 0;
 		
 		while (true) {
 			System.out.print("私が持っている数字はいくつでしょうか？ ");
 			Integer guess = new Scanner(System.in).nextInt();
+			count++;
 			
 			if (answer == guess) {
-				System.out.println("大正解！");
+				System.out.println("大正解！ " + count + " 回で正解できました！");
 				break;
 			}else if (answer > guess) {
 				System.out.println("もっと大きい数字です！");
