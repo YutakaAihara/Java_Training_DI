ad53fff6d1fd25ef80337f4fbff02a1811fec7eb
YutakaAihara <youguisufanyuan@gmail.com>
2021-08-15 20:02:31 +0900

    数値を受け取るように変更

--- src/guess_number.java
+++ src/guess_number.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class guess_number {
 	public static void main(String[] args) {
-		System.out.print("Your name? ");
-		String name = new Scanner(System.in).next();
+		System.out.print("私が持っている数字はいくつでしょうか？ ");
+		Integer guess = new Scanner(System.in).nextInt();
 		
-		System.out.println("Hello " + name );
-		System.out.println("Hello " + name + " Again!");
+		System.out.println("あなたの予想: " + guess);
 	}
 }
